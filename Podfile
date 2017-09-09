5886a0db8f177ab09b06516d8722d7558c3c812e
Jason.Liu <luemark@live.com>
2017-09-09 14:33:30 +0800

    commit

--- Podfile
+++
@@ -1,6 +0,0 @@
-platform :ios, '8.0'
-use_frameworks!
-
-target 'SSTextView' do
-  pod 'Masonry', '~> 1.0.2'
-end
--- Pods/Target Support Files/Pods-SSTextView/Pods-SSTextView-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_SSTextViewVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_SSTextViewVersionString[];
-
